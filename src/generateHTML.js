35c510a66aefa08753d1df8c40ede461bfa35809
Julianne <juliaguas17@gmail.com>
2023-04-25 16:41:36 -0500

    created Manager and Engineer cards

---
+++ src/generateHTML.js
@@ -0,0 +1,36 @@
+// Manager card
+const generateManager = function (manager) {
+    return `<div class="col-4 mt-4">
+                <div class="card h-100">
+                    <div class="card-header">
+                        <h3>${manager.name}</h3>
+                        <h4>Manager</h4><i class="material-icons">coffee</i>
+                    </div>
+                    <div class="card-body">
+                        <p class="id">ID: ${manager.id}</p>
+                        <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
+                        <p class="office">Office Number: ${manager.officeNumber}</p>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+
+// Engineer card
+const generateEngineer = function (engineer) {
+    return `<div class="col-4 mt-4">
+                <div class="card h-100">
+                    <div class="card-header">
+                        <h3>${engineer.name}</h3>
+                        <h4>Engineer</h4><i class="material-icons">engineering</i>
+                    </div>
+                    <div class="card-body">
+                        <p class="id">ID: ${engineer.id}</p>
+                        <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+                        <p class="github">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+
